1cffefbe6bee9f14bb3fffcc16b95f297d8f01df
Eugen Tudorancea <eugen.tudorancea@gmail.com>
2018-07-10 05:36:00 +0300

    Change hooks constructor arguments format.

--- packages/nore/source/util/hooks.js
+++ packages/nore/source/util/hooks.js
@@ -1,12 +1,18 @@
 import { SyncHook, AsyncSeriesHook } from "tapable";
 
 export class HookSync extends SyncHook {
+	constructor(...args) {
+		super(args);
+	}
 	add(plugin, handler) {
 		this.tap(plugin, handler);
 	}
 }
 
 export class HookAsync extends AsyncSeriesHook {
+	constructor(...args) {
+		super(args);
+	}
 	add(plugin, handler) {
 		this.tapPromise(plugin, handler);
 	}
