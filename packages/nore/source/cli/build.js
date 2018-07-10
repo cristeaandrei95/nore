2bc1405012770280f0f74a8abaffe9f97b2b4c10
Eugen Tudorancea <eugen.tudorancea@gmail.com>
2018-07-10 05:35:00 +0300

    nore:build add progress logs and delete output directories before compile.

--- packages/nore/source/cli/build.js
+++ packages/nore/source/cli/build.js
@@ -1,3 +1,4 @@
+import { deleteDirectory } from "@nore/std/fs";
 import { assign } from "@nore/std/object";
 import { join } from "@nore/std/path";
 import canLoadRequest from "../util/canLoadRequest";
@@ -7,6 +8,8 @@ import plugins from "../plugins";
 import bundles from "../bundles";
 
 export default async cli => {
+	log("nore:build", "in progress…");
+
 	const options = assign({}, cli, { mode: "production" });
 	const nore = new Platform(options);
 
@@ -21,17 +24,24 @@ export default async cli => {
 		if (!canLoadRequest(file)) continue;
 
 		await nore.loadBundle(options, config);
+
+		log("nore:build", `bundle: ${options.handle} – loaded`);
 	}
 
 	// compile bundles and watch for changes
 	for (const [_, bundle] of nore.bundles) {
 		if (bundle.isForWeb) {
+			// delete the brevious build
+			await deleteDirectory(bundle.output);
+
+			log("nore:build", `bundle: ${bundle.handle} – compile started`);
+
 			const stats = await bundle.compile();
 
 			if (stats.errors) {
 				log(stats.errors);
 			} else {
-				log("Compile completed");
+				log("nore:build", `bundle: ${bundle.handle} – compile finished`);
 			}
 		}
 	}
