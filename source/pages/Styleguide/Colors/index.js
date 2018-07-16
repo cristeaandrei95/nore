2b50bbd0e10dabf6cfb800292250e3806abe0e1b
Eugen Tudorancea <eugen.tudorancea@gmail.com>
2018-07-16 14:31:23 +0300

    Small code style tweaks.

--- source/pages/Styleguide/Colors/index.js
+++ source/pages/Styleguide/Colors/index.js
@@ -3,34 +3,34 @@ import Section from "../components/Section";
 import $ from "./style.css";
 
 export default () => (
-	<Section title="Colors" style={{ background: "#FFF" }}>
+	<Section title="Colors" style={{ background: "#FFFFFF" }}>
 		<b class={$.colors}>
 			<b class={$.color} style={{ backgroundColor: "#FFFEFC" }}>
-				#fffefc
+				#FFFEFC
 			</b>
 			<b class={$.color} style={{ backgroundColor: "#EEF2F5" }}>
-				#eef2f5
+				#EEF2F5
 			</b>
 			<b class={$.color} style={{ backgroundColor: "#4957B8" }}>
 				#4957B8
 			</b>
 			<b class={$.color} style={{ backgroundColor: "#F5F6F8" }}>
-				#f5f6f8
+				#F5F6F8
 			</b>
 			<b class={$.color} style={{ backgroundColor: "#F6F9FC" }}>
-				#f6f9fc
+				#F6F9FC
 			</b>
 			<b class={$.color} style={{ backgroundColor: "#FBFBFB" }}>
 				#FBFBFB
 			</b>
 			<b class={$.color} style={{ backgroundColor: "#F5F9FC" }}>
-				#f5f9fc
+				#F5F9FC
 			</b>
 			<b class={$.color} style={{ backgroundColor: "#E6F3F3" }}>
-				#e6f3f3
+				#E6F3F3
 			</b>
 			<b class={$.color} style={{ backgroundColor: "#F4F7FB" }}>
-				#f4f7fb
+				#F4F7FB
 			</b>
 		</b>
 	</Section>
