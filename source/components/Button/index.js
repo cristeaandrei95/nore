8fb50e543ea461819ce5109c34b9b8d4ff8864aa
Eugen Tudorancea <eugen.tudorancea@gmail.com>
2018-08-20 18:57:48 +0300

    Refactored buttons to allow focus and hover to have separate styling.

--- source/components/Button/index.js
+++ source/components/Button/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, createElement, cloneElement } from "react";
 import $ from "./style.css";
 
 const apply = (source, target, list) => {
@@ -10,49 +10,61 @@ const apply = (source, target, list) => {
 const isLink = el =>
 	el && el.type && (el.type === "a" || el.type.name === "Link");
 
-const fields = ["id", "className", "style"];
-const variants = ["default", "plain", "flat", "outlined", "raised"];
-const sizes = ["small", "large"];
-const states = ["active", "focus"];
-const modifiers = ["round", "disabled", "flexible"];
+const variants = ["default", "plain", "outlined", "flat", "raised"];
+const sizes = ["small", "medium", "large", "xlarge"];
+const states = ["hover", "active", "focus"];
+const modifiers = ["sharp", "round", "pill", "disabled"];
 const styles = ["primary", "secondary", "accent", "positive", "negative"];
-const events = ["onClick", "onBlur", "onFocus"];
 
 export default class Button extends Component {
 	isSet = key => this.props[key];
 
 	classes() {
 		const $variants = variants.filter(this.isSet);
+		const $sizes = sizes.filter(this.isSet);
+		const $states = states.filter(this.isSet);
+		const $styles = styles.filter(this.isSet);
+		const $modifiers = modifiers.filter(this.isSet);
 
 		if (!$variants.length) $variants.push("default");
+		if (!$sizes.length) $sizes.push("medium");
 
-		return $(
-			$variants,
-			sizes.filter(this.isSet),
-			states.filter(this.isSet),
-			styles.filter(this.isSet),
-			modifiers.filter(this.isSet)
-		);
+		return $($variants, $sizes, $states, $styles, $modifiers);
 	}
 
-	render({ children, label, disabled }, state) {
-		const attrs = {};
+	content() {
+		const { children, label, className, style } = this.props;
 
-		// add basic attributes
-		apply(this.props, attrs, fields);
+		const attrs = {
+			tabIndex: "-1",
+			className: className + this.classes(),
+			style: style,
+		};
 
-		// add events
-		if (!disabled) {
-			apply(this.props, attrs, events);
-		}
-
-		// join CSS classes
-		attrs.className += this.classes();
-
+		// if children is a link <a>, clone and extend
 		if (isLink(children)) {
-			return React.cloneElement(children, attrs);
+			return cloneElement(children, attrs);
 		}
 
-		return React.createElement("button", attrs, label || children);
+		return createElement("b", attrs, label || children);
+	}
+
+	render(props, state) {
+		const attrs = {
+			id: props.id,
+			tabIndex: "0",
+			className: $.reset,
+		};
+
+		// add events if not disabled
+		if (props.disabled) {
+			attrs.disabled = "disabled";
+		} else {
+			attrs.onClick = props.onClick;
+			attrs.onBlur = props.onBlur;
+			attrs.onFocus = props.onFocus;
+		}
+
+		return createElement("button", attrs, this.content());
 	}
 }
--- source/pages/Styleguide/Buttons/index.js
+++ source/pages/Styleguide/Buttons/index.js
@@ -5,44 +5,102 @@ import Section from "../components/Section";
 import Responsive from "../components/Responsive";
 import $ from "./style.css";
 
+const Group = ({ title, children }) => (
+	<b class={$.group}>
+		<b class={$.group_title}>{title}</b>
+		<b class={$.group_content}>{children}</b>
+	</b>
+);
+
 export default () => (
 	<Responsive title="Buttons">
-		<b class={$.group}>
-			<Button label="large" large accent />
+		<Group title="default / hover / active / focus">
 			<Button label="default" />
-			<Button label="focus" focus />
+			<Button label="hover" hover />
 			<Button label="active" active />
-
+			<Button label="focus" focus />
+		</Group>
+		<Group title="flat / hover / active / focus">
+			<Button label="flat" flat />
+			<Button label="hover" flat hover />
+			<Button label="active" flat active />
+			<Button label="focus" flat focus />
+		</Group>
+		<Group title="small / medium / large / xlarge">
 			<Button label="small" small />
-			<Button label="round" small round />
-			<Button label="disabled" small disabled />
-		</b>
-		<b class={$.group}>
-			<Button label="large" plain large />
-			<Button label="plain" plain />
-			<Button label="focus" plain focus />
-			<Button label="active" plain active />
-
-			<Button label="small" plain small />
-			<Button label="round" plain small round />
-			<Button label="disabled" plain small disabled />
-		</b>
-		<b class={$.group}>
-			<Button small>
-				<a href="#hashtag">#link</a>
-			</Button>
-			<Button small focus>
-				<a href="#hashtag">#focus</a>
-			</Button>
-			<Button small active>
-				<a href="#hashtag">#active</a>
-			</Button>
-			<Button small round>
+			<Button label="medium" />
+			<Button label="large" large />
+			<Button label="xlarge" xlarge />
+		</Group>
+		<Group title="sharp / round / pill / disabled">
+			<Button label="sharp" sharp />
+			<Button label="round" round />
+			<Button label="flat" flat />
+			<Button label="pill" pill />
+			<Button label="disabled" disabled />
+		</Group>
+		<Group title="primary / secondary / tertiary / positive / negative">
+			<Button label="primary" primary />
+		</Group>
+		<Group title="<Link> / <a>">
+			<Button round>
 				<Link to="/styleguide#buttons">link: internal</Link>
 			</Button>
-			<Button small round>
+			<Button round>
 				<a href="http://google.com">link: external</a>
 			</Button>
-		</b>
+			<Button round>
+				<a href="#hashtag">#link</a>
+			</Button>
+			<Button round active>
+				<a href="#hashtag">#active</a>
+			</Button>
+			<Button round focus>
+				<a href="#hashtag">#focus</a>
+			</Button>
+		</Group>
+		<Group title="onClick / onFocus / onBlur">
+			<Button label="on click" round onClick={e => console.log(e)} />
+			<Button label="on focus" round onFocus={e => console.log(e)} />
+			<Button label="on blur" round onBlur={e => console.log(e)} />
+		</Group>
 	</Responsive>
 );
+
+// <b class={$.group}>
+// 	<Button label="large" large accent />
+// 	<Button label="default" />
+// 	<Button label="focus" focus />
+// 	<Button label="active" active />
+
+// 	<Button label="small" small />
+// 	<Button label="round" small round />
+// 	<Button label="disabled" small disabled />
+// </b>
+// <b class={$.group}>
+// 	<Button label="large" plain large />
+// 	<Button label="plain" plain />
+// 	<Button label="focus" plain focus />
+// 	<Button label="active" plain active />
+
+// 	<Button label="small" plain small />
+// 	<Button label="round" plain small round />
+// 	<Button label="disabled" plain small disabled />
+// </b>
+// <b class={$.group}>
+// 	<Button small>
+// 		<a href="#hashtag">#link</a>
+// 	</Button>
+// 	<Button small focus>
+// 		<a href="#hashtag">#focus</a>
+// 	</Button>
+// 	<Button small active>
+// 		<a href="#hashtag">#active</a>
+// 	</Button>
+// 	<Button small round>
+// 		<Link to="/styleguide#buttons">link: internal</Link>
+// 	</Button>
+// 	<Button small round>
+// 		<a href="http://google.com">link: external</a>
+// 	</Button>
+// </b>
