70acc981969bd9a456f43dfe23fdfdcca06d65b7
Eugen Tudorancea <eugen.tudorancea@gmail.com>
2018-08-22 17:30:10 +0300

    Update page routes.

--- source/pages/index.js
+++ source/pages/index.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 import { Scope, Link } from "@nore/pwa";
 import Styleguide from "./Styleguide";
+import Typography from "./Styleguide/Typography";
+import Buttons from "./Styleguide/Buttons";
+import Colors from "./Styleguide/Colors";
+import HTML from "./Styleguide/HTML";
+import Ideas from "./Styleguide/Ideas";
 import Article from "./Styleguide/Typography/Article";
 
 export default context => (
@@ -8,11 +13,20 @@ export default context => (
 		<Scope exact match="/">
 			<b style={{ padding: "4rem" }}>
 				<Link to="/styleguide" label="Styleguide" /> <br />
-				<Link to="/typography/article" label="Typography / Article" />
+				<Link
+					to="/styleguide/typography/article"
+					label="Typography / Article"
+				/>
 			</b>
 		</Scope>
 
-		<Scope match="/styleguide" render={Styleguide} />
-		<Scope match="/typography/article" render={Article} />
+		<Scope exact match="/styleguide" render={Styleguide} />
+		<Scope exact match="/styleguide/buttons" render={Buttons} />
+		<Scope exact match="/styleguide/typography" render={Typography} />
+		<Scope exact match="/styleguide/colors" render={Colors} />
+		<Scope exact match="/styleguide/html" render={HTML} />
+
+		<Scope exact match="/styleguide/ideas" render={Ideas} />
+		<Scope exact match="/styleguide/typography/article" render={Article} />
 	</b>
 );
