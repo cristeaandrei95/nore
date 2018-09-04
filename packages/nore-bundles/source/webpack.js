9fd93dc889bf4b4382d13e1966cde86ab55939c3
Eugen Tudorancea <eugen.tudorancea@gmail.com>
2018-09-04 04:29:18 +0300

    Integrat nore-bundles plugin into core.

--- packages/nore-bundles/source/webpack.js
+++
@@ -1,5 +0,0 @@
-import webpack from "webpack";
-import merge from "webpack-merge";
-import { isArray } from "@nore/std/assert";
-
-export default config => webpack(isArray(config) ? merge(...config) : config);
