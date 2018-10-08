f978c716fca19a838a8d9facd3ffcaa7913312b6
Eugen Tudorancea <eugen.tudorancea@gmail.com>
2018-10-09 01:20:59 +0300

    Prefix "hidden" property on Nore instance.

--- packages/nore/source/util/Events.js
+++ packages/nore/source/util/Events.js
@@ -2,33 +2,33 @@
 export default class Emitter {
 	constructor() {
 		// store event handlers
-		this.events = new Map();
+		this.$events = new Map();
 	}
 
 	on(event, handler) {
-		const { events } = this;
+		const { $events } = this;
 
-		if (!events.has(event)) {
-			events.set(event, new Set());
+		if (!$events.has(event)) {
+			$events.set(event, new Set());
 		}
 
-		events.get(event).add(handler);
+		$events.get(event).add(handler);
 	}
 
 	off(event, handler) {
-		const { events } = this;
+		const { $events } = this;
 
-		if (!events.has(event)) {
-			return events.get(event).delete(handler);
+		if (!$events.has(event)) {
+			return $events.get(event).delete(handler);
 		}
 	}
 
 	emit(event, ...args) {
-		const { events } = this;
+		const { $events } = this;
 		const handlers = [];
 
-		if (events.has(event)) {
-			for (const handler of events.get(event)) {
+		if ($events.has(event)) {
+			for (const handler of $events.get(event)) {
 				handlers.push(handler(...args));
 			}
 		}
