aeb31c1a28bc27b57745e3251bd0a4c5b9157c41
Eugen Tudorancea <eugen.tudorancea@gmail.com>
2018-10-08 23:10:55 +0300

    Tidyup code.

--- packages/nore-fonts/source/webpackConfig.js
+++ packages/nore-fonts/source/webpackConfig.js
@@ -1,28 +1,28 @@
 export default bundle => {
+	const font = {
+		// Match woff2 in addition to patterns like .woff?v=1.1.1.
+		test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+		use: {
+			loader: "url-loader",
+			options: {
+				fallback: "file-loader",
+				mimetype: "application/font-woff",
+
+				// Limit at 50k. Above that it emits separate files
+				limit: 50000,
+
+				// output fonts path
+				name: "fonts/[name].[hash:8].[ext]",
+
+				// TODO: add URL public path
+				// publicPath: bundle.settings.url.pathname,
+			},
+		},
+	};
+
 	return {
 		module: {
-			rules: [
-				{
-					// Match woff2 in addition to patterns like .woff?v=1.1.1.
-					test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
-					use: {
-						loader: "url-loader",
-						options: {
-							fallback: "file-loader",
-							mimetype: "application/font-woff",
-
-							// Limit at 50k. Above that it emits separate files
-							limit: 50000,
-
-							// output fonts path
-							name: "fonts/[name].[hash:8].[ext]",
-
-							// TODO: add URL public path
-							// publicPath: bundle.config.url.pathname,
-						},
-					},
-				},
-			],
+			rules: [font],
 		},
 	};
 };
