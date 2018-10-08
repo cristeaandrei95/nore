0c7dfbc9c2c4426246a6b2b66adcc840d0f1d7b2
Eugen Tudorancea <eugen.tudorancea@gmail.com>
2018-10-09 01:21:09 +0300

    Code tityup.

--- packages/nore/source/plugins.js
+++ packages/nore/source/plugins.js
@@ -2,21 +2,20 @@ import variables from "@nore/nore-variables";
 import javascript from "@nore/nore-js";
 import css from "@nore/nore-css";
 import html from "@nore/nore-html";
-import images from "@nore/nore-images";
-import fonts from "@nore/nore-fonts";
 import md from "@nore/nore-md";
 import toml from "@nore/nore-toml";
 import yaml from "@nore/nore-yaml";
+import images from "@nore/nore-images";
+import fonts from "@nore/nore-fonts";
 
 export default [
-	// TODO: do we need to pass options to plugins?!
 	variables(),
 	javascript(),
 	css(),
 	html(),
-	images(),
-	fonts(),
 	md(),
 	toml(),
 	yaml(),
+	images(),
+	fonts(),
 ];
