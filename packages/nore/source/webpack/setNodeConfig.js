d3b53b5fd14d6293fa0aafb92333cf26c7853b4d
Eugen Tudorancea <eugen.tudorancea@gmail.com>
2018-10-09 18:31:03 +0300

    Add webpack-node-externals plugins to ignore modules from node_modules.
    Many packages have faulty implementations and weird CommonJS formats. To prevent errors, we ignore them.

--- packages/nore/source/webpack/setNodeConfig.js
+++ packages/nore/source/webpack/setNodeConfig.js
@@ -1,8 +1,16 @@
 import VirtualModules from "webpack-virtual-modules";
+import getNodeExternals from "webpack-node-externals";
 import nodeGlobalHelpers from "./nodeGlobalHelpers.js";
 
 export default (bundle, config) => {
 	config.output.filename = "index.js";
+
+	// ignore all modules from node_modules folder
+	config.externals = [
+		getNodeExternals({
+			whitelist: [/@nore.+/],
+		}),
+	];
 
 	config.plugins.push(
 		new VirtualModules({
