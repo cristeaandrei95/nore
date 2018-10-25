edaa85d6688752ab4f4dbf399d66446974d6c72e
Eugen Tudorancea <eugen.tudorancea@gmail.com>
2018-10-25 05:37:52 +0300

    Add error handling to loaders.

--- packages/nore-toml/source/loader.js
+++ packages/nore-toml/source/loader.js
@@ -1,7 +1,12 @@
 import toml from "toml";
 
-export default source => {
-	const content = toml.parse(source);
+export default function tomlLoader(source) {
+	try {
+		const content = toml.parse(source);
+		const module = JSON.stringify(content);
 
-	return "export default " + JSON.stringify(content);
-};
+		return `export default ${module}`;
+	} catch (error) {
+		this.emitError(error);
+	}
+}
--- packages/nore-yaml/source/loader.js
+++ packages/nore-yaml/source/loader.js
@@ -1,7 +1,12 @@
 import yaml from "js-yaml";
 
-export default source => {
-	const content = yaml.safeLoad(source);
+export default function yamlLoader(source) {
+	try {
+		const content = yaml.safeLoad(source);
+		const module = JSON.stringify(content);
 
-	return "export default " + JSON.stringify(content);
-};
+		return `export default ${module}`;
+	} catch (error) {
+		this.emitError(error);
+	}
+}
