96fa23d01ec93f4cff1a6c88ebc40cf1e1c2b391
Eugen Tudorancea <eugen.tudorancea@gmail.com>
2018-12-20 19:00:13 +0200

    Replace CSS loader webpack plugin.

--- packages/plugin-css/source/webpackConfig.js
+++ packages/plugin-css/source/webpackConfig.js
@@ -1,17 +1,11 @@
 import { merge } from "@nore/std";
-import CSSExtract from "mini-css-extract-plugin";
+import CSSExtract from "extract-css-chunks-webpack-plugin";
 import OptimizeCSS from "optimize-css-assets-webpack-plugin";
 import getLocalIdent from "./getLocalIdent.js";
-import postcss from "./postcss";
+import getPostCSSOptions from "./postcss";
 
-function getLoaders({ bundle, useModules }) {
-	const loaders = [`${__dirname}/classesLoader.js`];
-
-	if (bundle.isForWeb) {
-		loaders.push(bundle.isDevelopment ? "style-loader" : CSSExtract.loader);
-	}
-
-	loaders.push({
+async function getLoaders({ bundle, useModules }) {
+	const cssLoader = {
 		loader: "css-loader",
 		options: {
 			importLoaders: 1,
@@ -21,37 +15,37 @@ function getLoaders({ bundle, useModules }) {
 			getLocalIdent: useModules && getLocalIdent,
 			exportOnlyLocals: bundle.isForNode,
 		},
-	});
+	};
 
-	return loaders;
+	const postcssLoader = {
+		loader: "postcss-loader",
+		options: await getPostCSSOptions(bundle),
+	};
+
+	return [
+		`${__dirname}/classesLoader.js`,
+		CSSExtract.loader,
+		cssLoader,
+		postcssLoader,
+	];
 }
 
 export default async bundle => {
 	const { isDevelopment, isForWeb } = bundle;
-	const plugins = [];
 
-	// allow to load CSS Modules: `import style from "./style.css"`
-	const cssModulesLoaders = getLoaders({ bundle, useModules: true });
-
-	// allow to load raw CSS: `import "./bootstrap.css?raw`
-	const cssRawLoaders = getLoaders({ bundle, useModules: false });
-
-	const postcssLoader = {
-		loader: "postcss-loader",
-		options: await postcss(bundle),
-	};
-
-	const oneOf = [
-		{ use: [...cssRawLoaders, postcssLoader], resourceQuery: /raw/ },
-		{ use: [...cssModulesLoaders, postcssLoader] },
+	const plugins = [
+		new CSSExtract({
+			cssModules: true,
+			orderWarning: bundle.isDevelopment,
+			hot: bundle.isDevelopment,
+			reloadAll: bundle.isDevelopment,
+			filename: "[name].[contenthash].css",
+			chunkFilename: "[name].[contenthash].css",
+		}),
 	];
 
 	if (isForWeb && !isDevelopment) {
 		plugins.push(
-			new CSSExtract({
-				filename: "[name].[contenthash].css",
-				chunkFilename: "[name].[contenthash].css",
-			}),
 			new OptimizeCSS({
 				cssProcessorOptions: {
 					discardComments: { removeAll: true },
@@ -60,11 +54,23 @@ export default async bundle => {
 		);
 	}
 
+	// allow to load CSS Modules: `import style from "./style.css"`
+	const cssModulesLoaders = await getLoaders({ bundle, useModules: true });
+
+	// allow to load raw CSS: `import "./bootstrap.css?raw`
+	const cssRawLoaders = await getLoaders({ bundle, useModules: false });
+
+	const cssRule = {
+		test: /\.css$/,
+		oneOf: [
+			{ use: cssRawLoaders, resourceQuery: /raw/ },
+			{ use: cssModulesLoaders },
+		],
+	};
+
 	return {
 		plugins,
-		module: {
-			rules: [{ test: /\.css$/, oneOf }],
-		},
+		module: { rules: [cssRule] },
 		resolve: {
 			extensions: [".css"],
 		},
